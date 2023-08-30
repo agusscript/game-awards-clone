5d12993fbb7fcd6b2bcfe026cc519e412fdcf182
agusscript <agus.sanchez.240@gmail.com>
2023-08-30 11:52:47 -0300

    Add header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,21 +9,27 @@ function Header() {
   return (
     <>
       <header className="header">
-        <div className="header-wrapper">
-          <AwardsLogo className="header-logo" width={45} height={45} color="#ffffff" />
+        <div className="wrapper">
+
+          <AwardsLogo className="header-logo" width={45} height={45} color="whitesmoke" />
+
           <nav className="header-nav">
-            {header.nav.map((item) => (
-              <li className="header-li" key={item.id}>
-                <a className="header-a" href={item.href}>
-                  {item.title}
-                </a>
-              </li>
-            ))}
+            <ul className="header-ul">
+              {header.nav.map((item) => (
+                <li className="header-li" key={item.id}>
+                  <a className="header-a" href={item.href}>
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </nav>
+
           <button className="header-sign-btn">
             <img src="src\assets\login-icon.svg" alt="login icon" />
             SIGN IN
           </button>
+
         </div>
       </header>
     </>
