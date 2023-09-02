985fa3679bbe50bc5303478a4b96543672f1f294
agusscript <agus.sanchez.240@gmail.com>
2023-09-02 20:21:39 -0300

    Add home email contact section styled, add form validations with react hook form library

--- src/pages/Home/Email/Email.tsx
+++ src/pages/Home/Email/Email.tsx
@@ -1,7 +1,29 @@
-import MainButton from "../../../components/common/MainButton/MainButton";
 import "./Email.scss";
+import MainButton from "../../../components/common/MainButton/MainButton";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Email() {
+  const [visibleForm, setVisibleForm] = useState(true);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(userInfo: any) {
+    console.log(userInfo);
+    setVisibleForm(false);
+  }
+
+  const errorMessages = {
+    reqCheck: "you must agree the terms.",
+    reqEmail: "enter a valid email address.",
+  };
+
+  const emailRegexp = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+
   return (
     <section className="email">
       <div className="wrapper">
@@ -13,15 +35,54 @@ function Email() {
             <h2 className="content-subtitle">sign up to receive updates and news</h2>
           </div>
 
-          <form className="form">
+          <form
+            className={`form ${!visibleForm && "inactive"}`}
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <input className="form-input-name" type="text" placeholder="NAME" />
-            <input className="form-input-email" type="email" placeholder="EMAIL" />
+            <input
+              className="form-input-email"
+              type="text"
+              placeholder="EMAIL"
+              {...register("email", {
+                required: errorMessages.reqEmail,
+                pattern: { value: emailRegexp, message: errorMessages.reqEmail },
+              })}
+              name="email"
+            />
             <label className="form-label-check-agree" htmlFor="agree">
-              <input className="form-input-check-agree" type="checkbox" name="agree" />
-              <span className="check-span">you agree to receive occasional updates & news leading up the show</span>
+              <input
+                className="form-input-check-agree"
+                type="checkbox"
+                {...register("check", {
+                  required: errorMessages.reqCheck,
+                })}
+                name="check"
+              />
+              <span className="check-span">
+                you agree to receive occasional updates & news leading up the show
+              </span>
             </label>
-            <MainButton className="main-btn form-button" text="Submit" />
+
+            <MainButton
+              className="main-btn form-button"
+              text={errors.email || errors.cehck ? "Try again" : "Submit"}
+            />
+
+            <p className={`form-text-check-error ${errors.check && "active"}`}>
+              {errorMessages.reqCheck}
+            </p>
+            <p className={`form-text-email-error ${errors.email && "active"}`}>
+              {errorMessages.reqEmail}
+            </p>
           </form>
+
+          <div className={`success-container ${!visibleForm && "active"}`}>
+            <h2 className="success-container-title">Thanks for signing up!</h2>
+            <h3 className="success-container-subtitle">
+              You will receive a confirmation email shortly.
+            </h3>
+          </div>
         </div>
       </div>
     </section>
