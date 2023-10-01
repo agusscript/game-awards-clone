36afc38d9184f881b99f5341bc46622d67082e26
agusscript <agus.sanchez.240@gmail.com>
2023-10-01 15:32:52 -0300

    Add home transition overlay

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,10 +3,22 @@ import Intro from "./Intro/Intro";
 import News from "./News/News";
 import Explore from "./Explore/Explore";
 import Email from "./Email/Email";
+import { useEffect, useRef } from "react";
 
 function Home() {
+  const divOverlayRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("This will run after 4 second!");
+      divOverlayRef.current?.classList.add("hidden");
+    }, 3500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="home">
+      <div className="home-overlay" ref={divOverlayRef}></div>
       <Intro />
       <News />
       <Explore />
